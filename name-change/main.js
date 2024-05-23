c962ae51ec962b504578f6aa52ce9fc7afb2c51c
shamalelyas <shamalm3398@connect.durhamtch.edu>
2024-05-22 23:55:29 -0400

    add header navbar and navigetion

---
+++ name-change/main.js
@@ -0,0 +1,12 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector("nav");
+
+menuToggle.addEventListener("click", () => {
+  nav.classList.toggle("show");
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 768 && nav.classList.contains("show")) {
+    nav.classList.remove("show");
+  }
+});
